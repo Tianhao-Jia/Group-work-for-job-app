66648c9b4457400372a0fe1a37d08a6863fa6080
bowley <nt620579@dal.ca>
2022-01-25 19:28:47 -0400

    added buttons to MainActivity labeled as go to employee activity and go to employer activity as a mock login, we can change it later to be better, however also added Espresso testOpenEmployeeActivity() method that opens up the app, goes to MainActivity, clicks on go to employee activity and then transitions to the new activity. If the new activity is matching the appCompatTextView which is the current default text then the test passes. The test currently passes. Also modified the gradle build file.

---
+++ app/src/androidTest/java/com/example/myapplication/EmployeeEspressoTest.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import android.content.ComponentName;
+
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class EmployeeEspressoTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> myRule =
+            new ActivityScenarioRule<MainActivity>(MainActivity.class);
+
+
+    @Test
+    public void testOpenEmployeeActivity() {
+
+        onView(withId(R.id.debugGoToEmployeeActivity)).perform(click());
+        onView(withId(R.id.appCompatTextView)).check(matches(isDisplayed()));
+
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button employeeButton;
+    public Button employerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        employeeButton = (Button) findViewById(R.id.debugGoToEmployeeActivity);
+        employerButton = (Button) findViewById(R.id.debugGoToEmployerActivity);
+
+        employeeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EmployeeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
