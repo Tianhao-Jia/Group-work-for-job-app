b3f9212fbaac1a6e34994e016c22b788b47ab6b4
johncorsten <john.corsten@dal.ca>
2022-01-26 20:14:36 -0400

    Finished setting up firebase and wrote basic connection methods in MainActivity.java

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,11 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String FIREBASE_DATABASE_URL = "https://quick-cash-55715-default-rtdb.firebaseio.com/";
+    private FirebaseDatabase firebaseDB;
+    private DatabaseReference firebaseDBRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        connectFirebase();
+        writeToFirebaseRealTimeDB();
+    }
+
+    private void connectFirebase() {
+        firebaseDB = FirebaseDatabase.getInstance(FIREBASE_DATABASE_URL);
+        firebaseDBRef = firebaseDB.getReference("message");
+    }
+
+    private void writeToFirebaseRealTimeDB(){
+        // Just a test, can delete
+        firebaseDBRef.setValue("Hello Group 4");
     }
 }
