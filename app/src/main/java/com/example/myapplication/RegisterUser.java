db294d6863bc65165728fd57952246f738718897
Ted Graveson <ted.graveson@dal.ca>
2022-01-28 11:51:37 -0400

    Configuring RegisterUser activity for testing

--- app/src/main/java/com/example/myapplication/RegisterUser.java
+++ app/src/main/java/com/example/myapplication/RegisterUser.java
@@ -1,7 +1,28 @@
 package com.example.myapplication;
 
-public class RegisterUser {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterUser extends AppCompatActivity {
 
 
+    private static final String FIREBASE_DATABASE_URL = "https://quick-cash-55715-default-rtdb.firebaseio.com/";
+    private FirebaseDatabase firebaseDB;
+    private DatabaseReference firebaseDBRef;
 
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+        connectFirebase();
+    }
+
+    private void connectFirebase() {
+        firebaseDB = FirebaseDatabase.getInstance(FIREBASE_DATABASE_URL);
+        firebaseDBRef = firebaseDB.getReference("message");
+    }
 }
