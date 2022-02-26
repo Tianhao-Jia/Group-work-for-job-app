25f197db6b91338a77bd5ad10f0c509fef150576
johncorsten <john.corsten@dal.ca>
2022-02-26 14:09:13 -0500

    Added Job class.

---
+++ app/src/main/java/com/example/myapplication/Job.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Job extends AppCompatActivity {
+
+    private String employer_email;
+    private String job_title;
+    
+
+}
