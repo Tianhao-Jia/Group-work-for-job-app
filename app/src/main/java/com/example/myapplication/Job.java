16a8457822fd5c456cdedd2dc42173f02a438730
Ted Graveson <ted.graveson@dal.ca>
2022-02-26 15:28:54 -0400

    Draft job class

--- app/src/main/java/com/example/myapplication/Job.java
+++ app/src/main/java/com/example/myapplication/Job.java
@@ -1,12 +1,48 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AppComponentFactory;
 import android.os.Bundle;
 
 public class Job extends AppCompatActivity {
 
     private String employer_email;
     private String job_title;
-    
+    private String description;
 
+    /**
+     * Default constructor for AppCompatActivity. All Activities must have a default constructor
+     * for API 27 and lower devices or when using the default
+     * {@link AppComponentFactory}.
+     */
+    public Job(String employer_email, String job_title, String description) {
+        this.employer_email = employer_email;
+        this.job_title = job_title;
+        this.description = description;
+    }
+
+    public String getEmployer_email() {
+        return employer_email;
+    }
+
+    public void setEmployer_email(String employer_email) {
+        this.employer_email = employer_email;
+    }
+
+    public String getJob_title() {
+        return job_title;
+    }
+
+    public void setJob_title(String job_title) {
+        this.job_title = job_title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
