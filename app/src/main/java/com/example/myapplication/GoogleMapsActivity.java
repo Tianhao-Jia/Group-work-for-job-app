902bd2a84d4a3c0bc41aa63dda688dd18f2ff166
SaherPersonal <saher.ziauddin@dal.ca>
2022-03-02 23:13:44 -0400

    - created Google Maps Activity and xml file

---
+++ app/src/main/java/com/example/myapplication/GoogleMapsActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import com.example.myapplication.databinding.ActivityGoogleMapsBinding;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class GoogleMapsActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
