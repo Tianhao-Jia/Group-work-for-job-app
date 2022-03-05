c106dacee3ba42f1fce958a69f62698da6469a70
bowley <nt620579@dal.ca>
2022-03-05 15:42:41 -0400

    implemented setSearchButtonListener() method with java doc that is used to check to see if any of the EditTexts on the page is filled out and then populates a string[] array if it is, then that string array is passed to a new searchJobs method which is currently simply exits the program to confirm it is working. init() method that links EditTexts to the job_search.xml file was also added. with javadoc comments

--- app/src/main/java/com/example/myapplication/JobSearch.java
+++ app/src/main/java/com/example/myapplication/JobSearch.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 public class JobSearch extends Activity {
 
     private EditText employerEmailEditText;
@@ -13,22 +15,30 @@ public class JobSearch extends Activity {
     private EditText hourlyRateEditText;
     private Button searchButton;
 
-    //private static final String[] TEST = new String[]{ "Programmer", "Coder", "Miner", "Tester", "Problem Solver"};
-    //private HashMap<> hashMap;
+    private FirebaseDatabase firebaseDB;
+    private DatabaseReference jobsRef;
+
+    //stored in order of employerEmail, jobTitle, description, hourlyRate
+    private String[] inputs = new String[4];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.job_search);
-
         init();
 
-        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, TEST);
-        //AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.searchCreateJob);
-        //textView.setAdapter(adapter);
+        firebaseDB = FirebaseUtils.connectFirebase();
+        jobsRef = firebaseDB.getReference().child(FirebaseUtils.JOBS);
+
+        setSearchButtonListener();
 
     }
 
+    /**
+     * init method that links the parts of the job_search.xml file to the JobSearch.java fields.
+     * @author: Nathanael Bowley
+     */
     private void init() {
         employerEmailEditText = findViewById(R.id.searchEmployerEmail);
         jobTitleEditText = findViewById(R.id.searchJobTitle);
@@ -37,6 +47,46 @@ public class JobSearch extends Activity {
         searchButton = findViewById(R.id.searchJobButton);
     }
 
+    /**
+     * setSearchButtonListener method that gets the contents of the 4 editText boxes and stores them
+     * inside of the String[] array to determine if any inputs are to be checked, if all are left
+     * blank then no search will be conducted, however if even one is non empty then
+     * searchJobs method will be called.
+     * @author: Nathanael Bowley
+     */
+    private void setSearchButtonListener() {
+        searchButton.setOnClickListener(view -> {
+            inputs[0] = employerEmailEditText.getText().toString();
+            inputs[1] = employerEmailEditText.getText().toString();
+            inputs[2] = descriptionEditText.getText().toString();
+            inputs[3] = hourlyRateEditText.getText().toString();
 
+            boolean searchableString = false;
+            for (int i = 0; i< inputs.length; i++) {
+                if (!inputs[i].trim().isEmpty()) {
+                    searchableString = true;
+                }
+                else {
+                    inputs[i] = null;
+                }
+            }
+
+            if (searchableString) {
+                searchJobs(inputs);
+            }
+        });
+    }
+
+    /**
+     * searchJobs method that takes in an array of Strings as the searchPreferences and searches
+     * the firebase database to see if any of the chosen names or properties chosen match what the
+     * which then shows the corresponding searched inputs on the screen.
+     * user entered in.
+     * @param searchPreferences String[] array that is used as the search preferences of the user
+     * @author: Nathanael Bowley
+     */
+    private void searchJobs(String[] searchPreferences) {
+        System.exit(0);
+    }
 
 }
