dca4f9f59e6cee15e8dbb78d0b96325565cddef7
Ted Graveson <ted.graveson@dal.ca>
2022-03-07 21:48:34 -0400

    Starting job application functionality. Wills store in new section of database

--- app/src/main/java/com/example/myapplication/FirebaseUtils.java
+++ app/src/main/java/com/example/myapplication/FirebaseUtils.java
@@ -10,6 +10,7 @@ public class FirebaseUtils {
     public static final String FIREBASE_URL = "https://quick-cash-55715-default-rtdb.firebaseio.com/";
     public static final String USERS = "users";
     public static final String JOBS = "jobs";
+    public static final String APPS = "applications";
 
     public static FirebaseDatabase connectFirebase(){
         return FirebaseDatabase.getInstance(FirebaseUtils.FIREBASE_URL);
