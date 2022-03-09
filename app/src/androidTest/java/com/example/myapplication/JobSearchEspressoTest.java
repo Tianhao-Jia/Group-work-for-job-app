f83f2811d9e9e927253c27e379808e6aad3843ea
bowley <nt620579@dal.ca>
2022-03-09 09:53:10 -0400

    searchNoJobsExist espresso test passing now on JobSearchEspressoTest.java

--- app/src/androidTest/java/com/example/myapplication/JobSearchEspressoTest.java
+++ app/src/androidTest/java/com/example/myapplication/JobSearchEspressoTest.java
@@ -11,6 +11,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 import androidx.annotation.NonNull;
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -43,13 +44,29 @@ public class JobSearchEspressoTest {
     public static void tearDown() {
         Intents.release();
         FirebaseDatabase.getInstance().getReference("jobs").setValue(null);
+        FirebaseDatabase.getInstance().getReference("users").setValue(null);
     }
 
     @Test
     public void searchNoJobsExist(){
 
+        ActivityScenario.launch(EmployerActivity.class);
+
+        onView(withId(R.id.registerFirstName)).perform(typeText("George\n"));
+        onView(withId(R.id.registerLastName)).perform(typeText("Smith\n"));
+        onView(withId(R.id.registerEmail)).perform(typeText("george.smith@dal.ca\n"));
+        onView(withId(R.id.registerPasswordET)).perform(typeText("password123\n"));
+        onView(withId(R.id.registerUserType)).perform(typeText("Employer\n"));
+        Espresso.closeSoftKeyboard();
+
+        onView(withId(R.id.registerButton)).perform(click());
+
+        onView(withId(R.id.employerSearchButton)).perform(click());
 
         onView(withId(R.id.searchJobButton)).perform(click());
+
+        assertEquals(0,ViewJobAdapter.getHolderArrayList().size());
+
 
     }
 
