e7373b9ec17f4f73923b38d77b61e04fe4d96cd8
bowley <nt620579@dal.ca>
2022-03-27 01:11:44 -0300

    added jobEmployeeAdapter.java class and added onto Employee.java so that jobEmployerAdapter can make use of JobEmployeeAdapter

--- app/src/main/java/com/example/myapplication/Employee.java
+++ app/src/main/java/com/example/myapplication/Employee.java
@@ -17,7 +17,49 @@ package com.example.myapplication;
  */
 public class Employee {
 
+    private String firstName;
+    private String lastName;
+    private double rating;
+    private String email;
     public static String EMPLOYEE = "Employee";
 
+    public Employee(String firstName, String lastName, double rating, String email) {
+        this.firstName = firstName;
+        this.firstName = lastName;
+        this.rating = rating;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
 }
