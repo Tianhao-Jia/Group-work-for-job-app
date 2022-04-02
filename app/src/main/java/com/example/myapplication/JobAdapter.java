f7635ae28cb000aaedd31a27b6db14950701523f
tjia <tn608503@dal.ca>
2022-04-02 08:49:55 +0800

    adapter

--- app/src/main/java/com/example/myapplication/JobAdapter.java
+++ app/src/main/java/com/example/myapplication/JobAdapter.java
@@ -32,9 +32,12 @@ public class JobAdapter extends RecyclerView.Adapter<JobAdapter.ViewHolder> {
         Job job = list.get(position);
         Location location = job.getLocation();
         Log.d("testing:", location.getLatitude() + " " + location.getLongitude());
-        holder.tvTitle.setText(job.getJobTitle());
-        holder.tvDesc.setText(job.getDescription());
-        holder.tvLocation.setText("location:"+job.getLocation().getLatitude() +"-"+job.getLocation().getLongitude());
+        holder.tvTitle.setText("Job Title:"+job.getJobTitle());
+        holder.jobLayoutEmployerEmail.setText("Email:"+job.getEmployerEmail());
+        holder.tvDesc.setText("Hourly Rate: " +String.valueOf(job.getCompensation()));
+        holder.jobLayoutHourlyRate.setText(job.getHash());
+        holder.jobLayoutLatitude.setText("Latitude:"+job.getLocation().getLatitude());
+        holder.jobLayoutLongitude.setText("Longitude:"+job.getLocation().getLongitude());
         holder.tvJobCategory.setText(job.getCategory());
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,15 +56,21 @@ public class JobAdapter extends RecyclerView.Adapter<JobAdapter.ViewHolder> {
 
     class ViewHolder extends RecyclerView.ViewHolder{
         TextView tvTitle;
+        TextView jobLayoutEmployerEmail;
         TextView tvDesc;
-        TextView tvLocation;
+        TextView jobLayoutHourlyRate;
+        TextView jobLayoutLatitude;
+        TextView jobLayoutLongitude;
         TextView tvJobCategory;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            tvTitle = itemView.findViewById(R.id.payUserTitle);
-            tvDesc = itemView.findViewById(R.id.tvDesc);
-            tvLocation = itemView.findViewById(R.id.tvLocation);
-            tvJobCategory = itemView.findViewById(R.id.jobCategoryTV);
+            tvTitle = itemView.findViewById(R.id.jobLayoutJobTitle);
+            jobLayoutEmployerEmail = itemView.findViewById(R.id.jobLayoutEmployerEmail);
+            tvDesc = itemView.findViewById(R.id.jobLayoutDescription);
+            jobLayoutHourlyRate = itemView.findViewById(R.id.jobLayoutHourlyRate);
+            jobLayoutLongitude = itemView.findViewById(R.id.jobLayoutLongitude);
+            jobLayoutLatitude = itemView.findViewById(R.id.jobLayoutLatitude);
+            tvJobCategory = itemView.findViewById(R.id.jobCategory);
         }
     }
     public interface OnItemClickListener{
